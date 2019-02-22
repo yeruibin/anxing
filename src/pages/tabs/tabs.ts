75416cb75f820d07600aa6f2f702eafe36a6bc8b
Hanley <1374338350@qq.com>
2019-02-22 15:39:05 +0800

    Initial commit

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../health/about';
+import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
 
