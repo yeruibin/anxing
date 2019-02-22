ef701a0d266f9c453c85acb22570db6ca95e0d25
Hanley <1374338350@qq.com>
2019-02-22 15:55:21 +0800

    文件基本架构

---
+++ src/pages/find/find.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-find',
+  templateUrl: 'find.html'
+})
+export class FindPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { FindPage } from '../find/find';
+import { MinePage } from '../mine/mine';
+import { HealthPage } from '../health/health';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = HealthPage;
+  tab2Root = FindPage;
+  tab3Root = MinePage;
 
   constructor() {
 
